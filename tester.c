1d35946fffe8149eb115b4761184a326fb658358
nickrohn93@gmail.com <nickrohn93@gmail.com>
2015-11-05 22:49:24 -0700

    fixed bug

--- tester.c
+++ tester.c
@@ -3,8 +3,10 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <errno.h>
+#include <string.h>
 
-#define DEBUG 1 
+#define DEBUG 0 
 void debugp(char* msg) {if (DEBUG) fprintf(stderr, "%s\n", msg); return;}
 
 // first argument is how many procs to fork
@@ -12,33 +14,38 @@ void debugp(char* msg) {if (DEBUG) fprintf(stderr, "%s\n", msg); return;}
 // and then the arguments for given program
 int main(int argc, char* argv[]) {
 
-  pid_t cpid[argc];
   int i=0;
   int forkcount=0; 
   
-  char err[102500];
+  char err[1024];
   if (argc < 3) {
     fprintf(stderr, "usage: ./tester <# of forks> <program name> <program arguments*>\n");
-    exit(-1);
+    exit(0);
   }
 
   forkcount = atoi(argv[1]);
+  if (forkcount < 1) {
+    fprintf(stderr, "<# of forks> must be greater than 0");
+    exit(0);
+  }
   int status[forkcount];  
+  pid_t cpid[forkcount];
   sprintf(err, "trying to fork %d procs", forkcount);
   debugp(err);
   
   for (i=0; i < forkcount; ++i) {
     debugp("trying to fork");    
     cpid[i] = fork();
-    if (cpid[i] == 0) break; // child proc. we don't want n! behavior
+    if (cpid[i] == -1) {
+       fprintf(stderr, "error forking: %s\n", strerror(errno));
+       exit(-1);
+    }
+    if (cpid[i] == 0) {
+    execvp(argv[2], argv+2);
+    exit(errno);
+    }
   }
-  if (cpid[i] == 0) {
-    //sprintf(err, "\nI'm a child proc trying to execvp %s.\n", argv[2]);
-    //debugp(err);
-    execvp(argv[2], argv+3);
-    return 0;
-  }
-  else {
+
   debugp("\nentering main block\n");
   for (i=0; i < forkcount; ++i) {
     if (waitpid(cpid[i], &status[i], 0) != cpid[i]) {
@@ -47,10 +54,8 @@ int main(int argc, char* argv[]) {
   }
 
   for (i=0; i < forkcount; ++i) {
-    printf("\nproc %d returned with status %d\n", cpid[i], status[i]);
+    printf("\nproc %d returned with status %d\n", cpid[i], WEXITSTATUS(status[i]));
   }
    
   return 0;
-  }
-
 }
